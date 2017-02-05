7d3e8dbc6ed443a9475caede82c495b93e8d8e91
Aad Versteden <madnificent@gmail.com>
2017-02-05 17:41:33 +0100

    Transition to the show route when changes have persisted
    The easiest way to indicate that the changes have been sent to the
    server, is to transition to the show route.  This is our current
    approach.

--- app/controllers/articles/show/edit.js
+++ app/controllers/articles/show/edit.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     save: function() {
-      this.get('model').save();
+      this.get('model').save().then(
+        () => this.transitionToRoute("articles.show", this.get('model'))
+      );
     }
   }
 });
