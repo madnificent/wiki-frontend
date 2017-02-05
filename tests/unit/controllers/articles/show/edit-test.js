e34b110faceedc14dba6b3f66879a136316395a6
Aad Versteden <madnificent@gmail.com>
2017-02-05 17:34:19 +0100

    Implement basic articles/show/edit
    With this addition we have a basic way of editing existing articles.
    The implementation is very basic.  It provides us with a basic
    starting point.

---
+++ tests/unit/controllers/articles/show/edit-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:articles/show/edit', 'Unit | Controller | articles/show/edit', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
